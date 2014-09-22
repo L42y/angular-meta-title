938942033594d47f5436acb9511d7900be7e7e8b
L42y <423300@gmail.com>
2014-09-23 03:40:56 +0800

    refactor: put service into separate file

---
+++ angular-meta-title-service.js
@@ -0,0 +1,32 @@
+angular.module('l42y.meta.title').provider('Title', function () {
+  var api = {
+    name: 'title'
+  };
+  var defaults;
+  var service = {
+    options: {},
+    config: function (options) {
+      defaults = angular.copy(options);
+      service.options = options;
+      return service.options;
+    },
+    $get: function (
+      $window,
+      $rootScope
+    ) {
+      function setTitleContent (options) {
+        var op = service.options;
+        op = options ? angular.extend(op, options) : defaults;
+        api.content = $window.sprintf(op.format, op);
+
+        return api.content;
+      }
+
+      api.set = setTitleContent;
+
+      return api;
+    }
+  };
+
+  return service;
+});
--- angular-meta-title.js
+++ angular-meta-title.js
@@ -1,36 +1,5 @@
 angular.module('l42y.meta.title', [
-]).provider('Title', function () {
-  var api = {
-    name: 'title'
-  };
-  var defaults;
-  var service = {
-    options: {},
-    config: function (options) {
-      defaults = angular.copy(options);
-      service.options = options;
-      return service.options;
-    },
-    $get: function (
-      $window,
-      $rootScope
-    ) {
-      function setTitleContent (options) {
-        var op = service.options;
-        op = options ? angular.extend(op, options) : defaults;
-        api.content = $window.sprintf(op.format, op);
-
-        return api.content;
-      }
-
-      api.set = setTitleContent;
-
-      return api;
-    }
-  };
-
-  return service;
-}).run(function (
+]).run(function (
   $rootScope,
   Title
 ) {
